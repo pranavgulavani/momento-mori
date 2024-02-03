299476212c502e070e2300f5ac81f78352554faa
pranavgulavani <om2818@gmail.com>
2024-02-03 17:44:49 +0530

    basic form setup and dependencies added

--- src/components/momentoForm/MomentoForm.tsx
+++ src/components/momentoForm/MomentoForm.tsx
@@ -7,10 +7,40 @@ import {
   CardHeader,
 } from "../ui/card";
 import { Button } from "../ui/button";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { Input } from "../ui/input";
 
 type CardProps = React.ComponentProps<typeof Card>;
 
+const formSchema = z.object({
+  Birthday: z.string().min(2).max(50),
+});
+
 export default function MomentoForm({ className, ...props }: CardProps) {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      Birthday: "",
+    },
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
   return (
     <div className="flex-grow flex justify-center items-center">
       <Card className={cn("w-[380px]")}>
@@ -18,13 +48,30 @@ export default function MomentoForm({ className, ...props }: CardProps) {
           <CardDescription>Please fill the details</CardDescription>
         </CardHeader>
         <CardContent>
-          <p>Card Content</p>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <FormField
+                control={form.control}
+                name="Birthday"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Birthdate</FormLabel>
+                    <FormControl>
+                      <Input placeholder="birthday" {...field} type="date" />
+                    </FormControl>
+                    <FormDescription>
+                      This is your public display name.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button variant="default" className={cn("w-full")}>
+                Submit
+              </Button>
+            </form>
+          </Form>
         </CardContent>
-        <CardFooter>
-          <Button variant="default" className={cn("w-full")}>
-            Submit
-          </Button>
-        </CardFooter>
       </Card>
     </div>
   );
