427a64b0cd42f176e963746950f859079e68567c
pranavgulavani <om2818@gmail.com>
2024-02-24 17:13:39 +0530

    basic state configuraion

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
+import { useState } from "react";
 import "./App.css";
 import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
 import MomentoForm from "./components/momentoForm/MomentoForm";
 
 function App() {
+  const [data, setData] = useState({ weeksSpent: 0, weeksRemaining: 0 });
+
+  const handleSetData = (weekSpent: number, weeksRemaining: number) => {
+    setData((data) => ({
+      ...data,
+      weekSpent: weekSpent,
+      weeksRemaining: weeksRemaining,
+    }));
+  };
+
   return (
     <>
       <div className="flex flex-col max-h-svh h-screen">
         <Header />
-        <MomentoForm />
+        <MomentoForm handleResult={() => handleSetData} />
         <Footer />
       </div>
     </>
