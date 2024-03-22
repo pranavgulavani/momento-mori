ecc4d8a80697dc258b05be6c156957061dabf5aa
pranavgulavani <om2818@gmail.com>
2024-03-22 13:29:33 +0530

    Added switch component and timeline implementation.

--- src/components/timeline/Timeline.tsx
+++ src/components/timeline/Timeline.tsx
@@ -1,57 +1,29 @@
 type TimeLineProps = {
-  weeksSpent: number;
-  weeksRemaining: number;
+  monthsSpent: number;
+  monthsRemaining: number;
 };
 
-type CircleProps = {
-  cx: string;
-  cy: string;
-  r: string;
-  fill?: string;
-  id: string;
-};
-
-function Circle({ cx, cy, r, fill, id }: CircleProps) {
-  return <circle cx={cx} cy={cy} r={r} fill={fill} id={id} />;
-}
 export default function Timeline({
-  weeksSpent,
-  weeksRemaining,
+  monthsSpent,
+  monthsRemaining,
 }: TimeLineProps) {
   const renderCircles = () => {
     const circles = [];
-    let initialCx = 10;
-    let rowIndex = 10;
-    const timeline = weeksSpent + weeksRemaining;
-    console.log(timeline, weeksSpent, weeksRemaining);
+    const timeline = monthsSpent + monthsRemaining;
+
     for (let i = 1; i <= timeline; i++) {
       circles.push(
-        <Circle
+        <div
           key={i}
-          id={`${i}`}
-          cx={`${initialCx}`}
-          cy={`${rowIndex}`}
-          r="3"
-          fill={`${i <= weeksSpent ? "black" : "gray"}`}
-        />
+          className="w-[8px] md:w-[10px] h-[8px] md:h-[10px] border m-1 border-black "
+          style={{
+            backgroundColor: `${i <= monthsSpent ? "#0088FE" : ""}`,
+          }}
+        ></div>
       );
-      initialCx = initialCx + 10;
-      if (i % 52 === 0) {
-        initialCx = 10;
-        rowIndex += 10;
-      }
     }
     return circles;
   };
   const spentCircles = renderCircles();
-  return (
-    <>
-      <svg
-        viewBox="0 0 100% 100%"
-        className="w-full md:w-[40%] md:mx-auto  h-full border border-red-400"
-      >
-        {spentCircles}
-      </svg>
-    </>
-  );
+  return <>{spentCircles}</>;
 }
